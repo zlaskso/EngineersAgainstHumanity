269948cf8191b2eb2ae4cb90c6abd504bd470533
zlaskso <filip.zanton@gmail.com>
2025-12-07 21:38:56 +0100

    Visuella förändringar för bättre tydlighet vid språkbyte.

--- src/components/LanguageComponent.vue
+++ src/components/LanguageComponent.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="lang-switch" v-on:click="switchLanguage">
-    <span v-if="lang === 'sv'">🇸🇪</span>
-    <span v-else>🇬🇧</span>
+  <div class="lang-switch">
+    <span 
+      v-bind:class="{ 'opaque': lang !== 'en' }"
+      v-on:click="setLanguage('en')">
+      🇬🇧
+    </span>
+    <span 
+      v-bind:class="{ 'opaque': lang !== 'sv' }"
+      v-on:click="setLanguage('sv')">
+      🇸🇪
+    </span>
   </div>
 </template>
 
@@ -13,8 +21,10 @@ export default {
   },
   emits: ['switch-language'],
   methods: {
-    switchLanguage() {
-      this.$emit('switch-language');
+    setLanguage(selectedLanguage) {
+      if (this.lang !== selectedLanguage) {
+        this.$emit('switch-language');
+      }
     }
   }
 }
@@ -26,12 +36,28 @@ export default {
   top: 1rem;
   right: 1rem;
   font-size: 3rem;
-  cursor: pointer;
   display: flex;
+  gap: 15px;
   user-select: none;
   -webkit-user-select: none;
   z-index: 100;
 }
+
+span {
+  cursor: pointer;
+  transition: all 0.2s ease;
+}
+
+.opaque {
+  opacity: 0.3;
+  transform: scale(0.8);
+}
+
+span:hover {
+  opacity: 1;
+  transform: scale(1.1);
+}
+
 .lang-switch:active {
   transform: scale(0.9);
 }
--- src/views/LobbyView.vue
+++ src/views/LobbyView.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="lang-switch" v-on:click="switchLanguage">
-    <span v-if="lang === 'sv'">🇸🇪</span>
-    <span v-else>🇬🇧</span>
-  </div>
-  <h1>{{ uiLabels.gameCode }} {{ gameID }}</h1>
+  <h1>{{ uiLabels.lobbyView?.gameCode }} {{ gameID }}</h1>
   <!--
 
   <div>
@@ -24,10 +20,10 @@
   <h1>{{ gameSettings.lobbyName }}</h1>
 
   <section class="finalGameRules">
-  <h2>{{ uiLabels.gameRules }}</h2>
-  <p> {{ uiLabels.maxNumPlayers }} {{ gameSettings.maxPlayerAmount }}</p>
-  <p>{{ uiLabels.numOfRounds }} {{ gameSettings.numOfRounds }}</p>
-  <p> {{ uiLabels.cardsOnHand }} {{ gameSettings.cardsOnHand }}</p>
+  <h2>{{ uiLabels.createView?.gameRules }}</h2>
+  <p> {{ uiLabels.createView?.maxNumPlayers }} {{ gameSettings.maxPlayerAmount }}</p>
+  <p>{{ uiLabels.createView?.numOfRounds }} {{ gameSettings.numOfRounds }}</p>
+  <p> {{ uiLabels.createView?.cardsOnHand }} {{ gameSettings.cardsOnHand }}</p>
   </section>
 </template>
 
@@ -37,11 +33,15 @@ const socket = io("localhost:3000");
 
 export default {
   name: "LobbyView",
+  
+  props: {
+    uiLabels: Object
+  },
+
   data: function () {
     return {
       userName: "",
       gameID: "inactive poll",
-      uiLabels: {},
       joined: false,
       participants: [],
       gameSettings: {
@@ -54,11 +54,9 @@ export default {
   },
   created: function () {
     this.gameID = this.$route.params.id;
-    socket.on("uiLabels", (labels) => (this.uiLabels = labels));
     socket.on("participantsUpdate", (p) => (this.participants = p));
     socket.on("startPoll", () => this.$router.push("/poll/" + this.gameID));
     socket.emit("joinPoll", this.gameID);
-    socket.emit("getUILabels", this.lang);
     socket.emit("getGameSettings", this.gameID);
     socket.on("gameSettings", (room) => {
     if (room) {
@@ -71,15 +69,6 @@ export default {
       socket.emit("participateInPoll", { gameID: this.gameID, name: this.userName });
       this.joined = true;
     },
-    switchLanguage: function () {
-      if (this.lang === "en") {
-        this.lang = "sv";
-      } else {
-        this.lang = "en";
-      }
-      localStorage.setItem("lang", this.lang);
-      socket.emit("getUILabels", this.lang);
-    },
     toggleNav: function () {
       this.hideNav = !this.hideNav;
     },
@@ -88,18 +77,4 @@ export default {
 </script>
 
 <style scoped>
-.lang-switch {
-  position: absolute;
-  top: 1rem;
-  right: 1rem;
-  font-size: 3rem;
-  cursor: pointer;
-  display: flex;
-  user-select: none;
-  user-select: none;
-  -webkit-user-select: none;
-}
-.lang-switch:active {
-  transform: scale(0.9);
-}
 </style>
